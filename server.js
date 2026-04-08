d390251c45f636ea88c0e4bc6ef0d8122890d3b4
fco200 <carlosfrancoaguayo44@gmail.com>
2026-04-08 03:30:36 -0700

    Eliminar declaración duplicada de cors

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const cors = require('cors');
-app.use(cors()); // Esto permite que cualquier origen (como GitHub Pages) se conecte
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const cors = require('cors'); // Solo una vez
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 
 mongoose.connect(process.env.MONGO_URI)
