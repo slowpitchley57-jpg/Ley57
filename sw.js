3e61ed1b5d848951208b46c74fd82625cecc314a
Fco200 <carlosfrancoaguayo44@gmail.com>
2026-05-10 17:55:55 -0700

    pagina lista3

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'ley57-v1';
+const assets = [
+  '/',
+  '/index_post.html',
+  '/logoalvnegro.jpeg',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
